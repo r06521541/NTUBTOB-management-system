d095553c77c1bf02a0bb840c80fd6d7e620c1b4b
YungHsiu <yunghsiu1994@gmail.com>
2024-05-30 22:06:02 +0800

    chore: use pip3

--- makes/shared_lib.mk
+++ makes/shared_lib.mk
@@ -3,4 +3,4 @@ SHARED_LIB_DIR = shared_lib
 build-and-install-shared-lib:
 	@echo "Building shared library..."
 	@cd $(SHARED_LIB_DIR) && python setup.py sdist
-	pip install shared_lib/dist/shared_lib-0.0.1.tar.gz
+	pip3 install shared_lib/dist/shared_lib-0.0.1.tar.gz
