010698887d70b325549b3d172f5436fdf71baa2d
Po-An Chen <r06521541@ntu.edu.tw>
2024-06-12 00:08:13 +0800

    chore: fix the name of make used in deploy.mk.

--- makes/deploy.mk
+++ makes/deploy.mk
@@ -1,8 +1,8 @@
 SHARED_LIB_VERSION = 0.0.1
 
 deploy-notify-token-service-add:
-	make build-shared-lib
-	midir -p functions/notify_token_service/dist
+	make build-and-install-shared-lib
+	mkdir -p functions/notify_token_service/dist
 	cp $(SHARED_LIB_DIR)/dist/shared_lib-${SHARED_LIB_VERSION}.tar.gz \
 		functions/notify_token_service/dist/shared_lib-${SHARED_LIB_VERSION}.tar.gz
 	gcloud functions deploy notify_token_service_add \
@@ -16,7 +16,7 @@ deploy-notify-token-service-add:
 		--source functions/notify_token_service/
 
 deploy-notify-token-service-get:
-	make build-shared-lib
+	make build-and-install-shared-lib
 	mkdir -p functions/notify_token_service/dist
 	cp $(SHARED_LIB_DIR)/dist/shared_lib-${SHARED_LIB_VERSION}.tar.gz \
 		functions/notify_token_service/dist/shared_lib-${SHARED_LIB_VERSION}.tar.gz
@@ -31,7 +31,7 @@ deploy-notify-token-service-get:
 		--source functions/notify_token_service/
 
 deploy-weekly-game-notify:
-	make build-shared-lib
+	make build-and-install-shared-lib
 	mkdir -p functions/weekly_game_notify/dist
 	cp $(SHARED_LIB_DIR)/dist/shared_lib-${SHARED_LIB_VERSION}.tar.gz \
 		functions/weekly_game_notify/dist/shared_lib-${SHARED_LIB_VERSION}.tar.gz
