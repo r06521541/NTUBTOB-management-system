2eb9fbfd0ea48b706b647959524287afef8b9bda
Po-An Chen <r06521541@ntu.edu.tw>
2024-07-11 00:13:36 +0800

    chore: delete the duplicate folder.

--- functions/weekly_game_notify copy/envs.py
+++
@@ -1,7 +0,0 @@
-import os
-
-team_name = os.environ.get("TEAM_NAME")
-game_crawl_api = os.environ.get("GAME_CRAWL_API")
-notify_api = os.environ.get("NOTIFY_API")
-notify_token_id = os.environ.get("NOTIFY_TOKEN_ID")
-notify_alarm_token_id = os.environ.get("NOTIFY_ALARM_TOKEN_ID")
