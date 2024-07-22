621deed8c701bd17d2143ec30bb1b91319644012
Po-An Chen <r06521541@ntu.edu.tw>
2024-07-23 02:05:01 +0800

    chore: rename as NOTIFY_SUCCESS_TOKEN_ID

--- functions/update_game_schedule/envs.py
+++ functions/update_game_schedule/envs.py
@@ -1,7 +1,7 @@
 import os
 
 notify_api = os.environ.get("NOTIFY_API")
-notify_token_id = os.environ.get("NOTIFY_TOKEN_ID")
+notify_success_token_id = os.environ.get("NOTIFY_SUCCESS_TOKEN_ID")
 notify_alarm_token_id = os.environ.get("NOTIFY_ALARM_TOKEN_ID")
 
 game_crawl_api = os.environ.get("GAME_CRAWL_API")
